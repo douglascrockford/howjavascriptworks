fdb616e2a1b8d88dc4f93ab18f79c51517be813b
stefanhamburger <9825318+stefanhamburger@users.noreply.github.com>
2018-11-06 01:27:06 +0100

    Update date

--- big_rational.js
+++ big_rational.js
@@ -1,6 +1,6 @@
 // big_rational.js
 // Douglas Crockford
-// 2018-09-24
+// 2018-11-05
 
 // You can access the big rational object in your module by importing it.
 //      import big_rational from "./big_rational.js";
